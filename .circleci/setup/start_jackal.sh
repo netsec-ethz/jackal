0a58bcc0fafe8bae1f7232a77c6d1975bdd2d172
Milos Malesevic <malesevic.milos2@gmail.com>
2019-07-03 08:55:53 +0200

    Adding second jackal server to the tests

--- .circleci/setup/start_jackal.sh
+++ .circleci/setup/start_jackal.sh
@@ -21,6 +21,13 @@ while ! mysqladmin ping -u root -h localhost -ppassword --silent; do
 done
 echo "GRANT ALL ON jackal.* TO 'jackal'@'localhost' IDENTIFIED BY 'password';" | mysql -h localhost -u root -ppassword
 echo "CREATE DATABASE jackal;" | mysql -h localhost -u jackal -ppassword
-cp -r ~/go/src/github.com/ortuman/jackal/testdata/s2_data/* .
-mysql -h localhost -D jackal -u jackal -ppassword < server2.sql
-./jackal -c example.jackal.yml
+if [ "$JACKAL_ID" == "$server1" ]; then
+    echo "Jackal server 1 data"
+    cp -r ~/go/src/github.com/ortuman/jackal/testdata/s1_data/* .
+    mysql -h localhost -D jackal -u jackal -ppassword < server1.sql
+else
+    echo "Jackal server 2 data"
+    cp -r ~/go/src/github.com/ortuman/jackal/testdata/s2_data/* .
+    mysql -h localhost -D jackal -u jackal -ppassword < server2.sql
+fi
+./jackal -c example.jackal.yml </dev/null &>/dev/null &
