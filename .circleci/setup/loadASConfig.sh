50c3293f63278482e67ee97cff3a9412b6a0a97c
Milos Malesevic <malesevic.milos2@gmail.com>
2019-07-24 23:44:55 +0200

    CI - getting rid of theSCIONLab coordinator

--- .circleci/setup/loadASConfig.sh
+++ .circleci/setup/loadASConfig.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 
-set -e
-
 # Get configuration
-curl --fail -u ${CUSER}:${CSECRET} http://coord:8000/api/host/${CUSER}/config -o /tmp/host_config.tar
 rm $SC/gen -rf
 tar -C $SC/ -xf /tmp/host_config.tar
 
@@ -16,4 +13,3 @@ cd $SC
 ./supervisor/supervisor.sh stop all
 ./supervisor/supervisor.sh reload
 ./supervisor/supervisor.sh start all
-
