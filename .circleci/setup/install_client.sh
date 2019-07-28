c598116ea5b5d4556c863554302168c9da40f87c
Milos Malesevic <malesevic.milos2@gmail.com>
2019-07-29 01:20:13 +0200

    CI - client1 successfully started

--- .circleci/setup/install_client.sh
+++ .circleci/setup/install_client.sh
@@ -6,13 +6,14 @@ apt-get install -y profanity
 
 # Set up the profanity account
 mkdir --parent /root/.local/share/profanity
-cp /home/c1_data/profanity/* /root/.local/share/profanity/
+cp -r /home/c1_data/profanity/* /root/.local/share/profanity/
 
 if [ "$CLIENT_ID" == "client1" ]; then
     echo "172.31.0.111 server1.xmpp" >> /etc/hosts
     cp /home/c1_data/server1.xmpp.crt /usr/local/share/ca-certificates/
     update-ca-certificates
-    profanity -a user1 </dev/null &>/dev/null &
 else
-    profanity -a user2 </dev/null &>/dev/null &
+    echo "172.31.0.112 server2.xmpp" >> /etc/hosts
+    cp /home/c2_data/server2.xmpp.crt /usr/local/share/ca-certificates/
+    update-ca-certificates
 fi
