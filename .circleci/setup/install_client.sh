77f562d35bcca8398007eb88c262cdb9ef27387b
Milos Malesevic <malesevic.milos2@gmail.com>
2019-07-28 18:03:49 +0200

    CI - bug fix in client install script

--- .circleci/setup/install_client.sh
+++ .circleci/setup/install_client.sh
@@ -8,7 +8,7 @@ apt-get install -y profanity
 mkdir --parent /root/.local/share
 mv /home/c1_data/profanity /root/.local/share/
 
-if ["$CLIENT_ID" == "client1"]; then
+if [ "$CLIENT_ID" == "client1" ]; then
     echo "172.31.0.111 server1.xmpp" >> /etc/hosts
     cp /home/c1_data/server1.xmpp.crt /usr/local/share/ca-certificates/
     update-ca-certificates
