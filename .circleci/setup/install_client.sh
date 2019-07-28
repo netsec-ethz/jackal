c4950d07fe8c61d7967669a4c48b1e45c05c9e60
Milos Malesevic <malesevic.milos2@gmail.com>
2019-07-28 17:54:24 +0200

    CI - adding XMPP client

---
+++ .circleci/setup/install_client.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Installing profanity client
+apt-get update -y
+apt-get install -y profanity
+
+# Set up the profanity account
+mkdir --parent /root/.local/share
+mv /home/c1_data/profanity /root/.local/share/
+
+if ["$CLIENT_ID" == "client1"]; then
+    echo "172.31.0.111 server1.xmpp" >> /etc/hosts
+    cp /home/c1_data/server1.xmpp.crt /usr/local/share/ca-certificates/
+    update-ca-certificates
+    profanity -a user1 </dev/null &>/dev/null &
+else
+    profanity -a user2 </dev/null &>/dev/null &
+fi
