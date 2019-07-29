0e2c3664e75671cd3647a17b9f9a8136e2fe2a81
Milos Malesevic <malesevic.milos2@gmail.com>
2019-07-29 05:04:41 +0200

    CI - running RAINS in infra1305

--- .circleci/setup/start_jackal.sh
+++ .circleci/setup/start_jackal.sh
@@ -11,6 +11,13 @@ else
     echo "Jackal server 2 data"
     cp -r ~/go/src/github.com/ortuman/jackal/testdata/s2_data/* .
     mysql -h localhost -D jackal -u jackal -ppassword < server2.sql
+
+    cd ~/jackal/rainsd
+    ./keymanager gen
+    ./keymanager selfsign -s selfSignedRootDelegationAssertion.gob
+    ./rainsd testdata/conf/SCIONnamingServerRoot.conf </dev/null &>/dev/null &
+    ./publisher testdata/conf/SCIONpublisherXmpp.conf
+    echo "19-ffaa:0:1305,[127.0.0.1]:5022" >> /home/ubuntu/go/src/github.com/scionproto/scion/gen/rains.cfg
 fi
 
 ./jackal -c example.jackal.yml </dev/null &>/dev/null &
