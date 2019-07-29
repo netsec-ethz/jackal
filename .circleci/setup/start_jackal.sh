9ec48561c0bdf2c16b45b45a94bb64c83a1fbc7c
Milos Malesevic <malesevic.milos2@gmail.com>
2019-07-29 07:03:32 +0200

    CI - using /etc/hosts instead of RAINS

--- .circleci/setup/start_jackal.sh
+++ .circleci/setup/start_jackal.sh
@@ -12,12 +12,12 @@ else
     cp -r ~/go/src/github.com/ortuman/jackal/testdata/s2_data/* .
     mysql -h localhost -D jackal -u jackal -ppassword < server2.sql
 
-    cd ~/jackal/rainsd
-    ./keymanager gen
-    ./keymanager selfsign -s selfSignedRootDelegationAssertion.gob
-    ./rainsd testdata/conf/SCIONnamingServerRoot.conf </dev/null &>/dev/null &
-    ./publisher testdata/conf/SCIONpublisherXmpp.conf
-    echo "19-ffaa:0:1305,[127.0.0.1]:5022" >> /home/scion/go/src/github.com/scionproto/scion/gen/rains.cfg
+    #cd ~/jackal/rainsd
+    #./keymanager gen
+    #./keymanager selfsign -s selfSignedRootDelegationAssertion.gob
+    #./rainsd testdata/conf/SCIONnamingServerRoot.conf </dev/null &>/dev/null &
+    #./publisher testdata/conf/SCIONpublisherXmpp.conf
+    #echo "19-ffaa:0:1305,[127.0.0.1]:5022" >> /home/scion/go/src/github.com/scionproto/scion/gen/rains.cfg
 fi
 
 cd ~/jackal
