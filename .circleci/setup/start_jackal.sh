9d54c766faa3405fde3a98de31d765903a39f93e
Milos Malesevic <malesevic.milos2@gmail.com>
2019-07-29 17:38:59 +0200

    CI - cleanup

--- .circleci/setup/start_jackal.sh
+++ .circleci/setup/start_jackal.sh
@@ -7,19 +7,12 @@ echo "GRANT ALL ON jackal.* TO 'jackal'@'localhost' IDENTIFIED BY 'password';" |
 echo "CREATE DATABASE jackal;" | mysql -h localhost -u jackal -ppassword
 if [ "$JACKAL_ID" == "server1" ]; then
     echo "Jackal server 1 data"
-    cp -r ~/go/src/github.com/ortuman/jackal/testdata/s1_data/* .
+    cp -r ~/go/src/github.com/ortuman/jackal/.circleci/testdata/s1_data/* .
     mysql -h localhost -D jackal -u jackal -ppassword < server1.sql
 else
     echo "Jackal server 2 data"
-    cp -r ~/go/src/github.com/ortuman/jackal/testdata/s2_data/* .
+    cp -r ~/go/src/github.com/ortuman/jackal/.circleci/testdata/s2_data/* .
     mysql -h localhost -D jackal -u jackal -ppassword < server2.sql
-
-    #cd ~/jackal/rainsd
-    #./keymanager gen
-    #./keymanager selfsign -s selfSignedRootDelegationAssertion.gob
-    #./rainsd testdata/conf/SCIONnamingServerRoot.conf </dev/null &>/dev/null &
-    #./publisher testdata/conf/SCIONpublisherXmpp.conf
-    #echo "19-ffaa:0:1305,[127.0.0.1]:5022" >> /home/scion/go/src/github.com/scionproto/scion/gen/rains.cfg
 fi
 
 cd ~/jackal
