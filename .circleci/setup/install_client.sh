44e40e55266c42630ebc665cd9ec864c251a0d27
Juan A. Garcia Pardo <juagargi@gmail.com>
2019-10-29 11:43:01 +0100

    Simplify CI

--- .circleci/setup/install_client.sh
+++ .circleci/setup/install_client.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-
-# Installing profanity client
-apt-get update -y
-apt-get install -y profanity
+set -xe
 
 # Set up the profanity account
-mkdir --parent /root/.local/share/profanity
-
 if [ "$CLIENT_ID" == "client1" ]; then
-    cp -r /home/c1_data/profanity/* /root/.local/share/profanity/
+    cp -r /root/c1_data/profanity/* /root/.local/share/profanity/
     echo "172.31.0.111 server1.xmpp" >> /etc/hosts
-    cp /home/c1_data/server1.xmpp.crt /usr/local/share/ca-certificates/
+    cp /root/c1_data/server1.xmpp.crt /usr/local/share/ca-certificates/
     update-ca-certificates
 else
-    cp -r /home/c2_data/profanity/* /root/.local/share/profanity/
+    cp -r /root/c2_data/profanity/* /root/.local/share/profanity/
     echo "172.31.0.112 server2.xmpp" >> /etc/hosts
-    cp /home/c2_data/server2.xmpp.crt /usr/local/share/ca-certificates/
+    cp /root/c2_data/server2.xmpp.crt /usr/local/share/ca-certificates/
     update-ca-certificates
 fi
